a4608f72daf79a90aeed37a749fd88b0ddc88d3d
Sümeyye Turan <36699340+SumeyyeTurann@users.noreply.github.com>
2020-09-20 20:33:49 +0300

    fatura
    .

---
+++ FaturaTakipSistemi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FaturaTakipSistemi.MvcApplication" Language="C#" %>
---
+++ FaturaTakipSistemi/Models/Sınıf/Class1.cs
@@ -0,0 +1,18 @@
+﻿using FaturaTakipSistemi.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaturaTakipSistemi.Models.Sınıf
+{
+    public class Class1
+    {
+        public IEnumerable<kullanici> kullanici { get; set; }
+        public IEnumerable<Elektrik> elektrik { get; set; }
+        public IEnumerable<Internet> internet { get; set; }
+        public IEnumerable<Su> su { get; set; }
+        public IEnumerable<Telefon> telefon { get; set; }
+        public IEnumerable<DGaz> dgaz { get; set; }
+    }
+}
